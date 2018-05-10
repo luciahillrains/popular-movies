e874a54602d566a69255b6ae52a6bb2eb4bbc066
Lucia Hill-Rains <luciahillrains@outlook.com>
2018-05-10 05:37:20 -0500

    refactor to have view holder have a relativelayout; got a grid

--- app/src/main/java/lucyhill/popularmovies/adapters/PosterViewAdapter.java
+++ app/src/main/java/lucyhill/popularmovies/adapters/PosterViewAdapter.java
@@ -7,20 +7,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.squareup.picasso.Picasso;
 
-import java.util.List;
 
 import lucyhill.popularmovies.R;
 import lucyhill.popularmovies.objects.PaginatedMovieList;
 
 public class PosterViewAdapter extends RecyclerView.Adapter<PosterViewAdapter.ViewHolder>  {
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        public ImageView mPoster;
-        public ViewHolder(ImageView v) {
+        public RelativeLayout view;
+        public ViewHolder(RelativeLayout v) {
             super(v);
-            mPoster = v;
+            view = v;
         }
     }
     public static final String IMAGES_BASE_URL = "https://image.tmdb.org/t/p/w500";
@@ -34,14 +34,14 @@ public class PosterViewAdapter extends RecyclerView.Adapter<PosterViewAdapter.Vi
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         String posterImage = mDataSet.getResults().get(position).getPosterPath();
         Log.d("PosterViewAdapter", "poster view path is "+posterImage );
-
-        Picasso.get().load(IMAGES_BASE_URL+posterImage).into(holder.mPoster);
+        ImageView imageView = holder.view.findViewById(R.id.movie_poster);
+        Picasso.get().load(IMAGES_BASE_URL+posterImage).into(imageView);
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        ImageView view = (ImageView) LayoutInflater.from(parent.getContext()).inflate(R.layout.poster_view_item, parent, false);
+        RelativeLayout view = (RelativeLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.poster_view_item, parent, false);
         ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
     }
